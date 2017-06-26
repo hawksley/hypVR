78e4c3ba5580d4c4ccfc6685cefaec3a6c4d4ab9
ELLIE\Andrea <hawksley@gmail.com>
2017-06-26 11:42:13 -0700

    undo new api temporarily

--- js/vr/VRControlsHyperbolic.js
+++ js/vr/VRControlsHyperbolic.js
@@ -181,10 +181,8 @@ THREE.VRControls = function ( camera, done ) {
 				position = vrInput.getState().position;
 				position = [position.x, position.y, position.z];
 			} else {
-				var frameData = new VRFrameData();
-				vrInput.getFrameData(frameData);
-				position = frameData.pose.position;
-				orientation = frameData.pose.orientation;
+				orientation    = vrInput.getPose().orientation;
+                position = vrInput.getPose().position;
 			}
 		} else if (this.phoneVR.rotationQuat()) {
 			var rotation = this.phoneVR.rotationQuat();
