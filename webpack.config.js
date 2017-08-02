5e27d7ca3d53ca64fae45db5916a2e4219406c3f
Abdoul Sy <abdoul.sy@gamesys.co.uk>
2017-08-02 11:45:39 +0100

    [WIP-es2017-conversion] Converted PhoneVR to the class syntax

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
      output: {
          path: path.resolve(__dirname, './dist'),
          filename: 'index.bundle.js'
-     }
+     },
+     module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2017']
+            }
+          }
+        }
+      ]
+    }
  };
