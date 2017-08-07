bf860190362f949764c2b8ed6d25c520d142355f
Abdoul Sy <abdoul.sy@gamesys.co.uk>
2017-08-07 01:24:29 +0100

    [Hotfix] Making polyfill into the bundled application to use it on Android

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
 window.THREE = require('three');
+import WebVRPolyfill from 'webvr-polyfill';
+
+// Dynamically turn the polyfill on if requested by the query args.
+if (WGLUUrl.getBool('polyfill', false)) {
+  InitializeWebVRPolyfill();
+} else {
+  // Shim for migration from older version of WebVR. Shouldn't be necessary for very long.
+  InstallWebVRSpecShim();
+}
 
 //VRControlsHyperbolic.js acquires positional information from connected VR devices and applies the transformations to a three.js camera object. Also deals with hyperbolic and parabolic motions.
 require('./three-extras/VRControlsHyperbolic.js');
